ee4bacf62157c49942d1b67a0b753af2e1976c83
sherinaeria <hastalinas@gmail.com>
2021-03-20 10:29:04 +0700

    streamlit app

---
+++ app.py
@@ -0,0 +1,126 @@
+# core pkg
+import streamlit as st
+
+# EDA pkg
+import pandas as pd
+import plotly.express as px 
+
+# DB
+from db_fxn import *
+
+def main():
+    st.title("To do app with streamlit")
+
+    menu = ["Create","Read","Update","Delete","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    create_table()
+    if choice == "Create":
+        st.subheader("Add Items")
+
+        # Layout
+        col1, col2 = st.beta_columns(2)
+
+        with col1:
+            task = st.text_area("Task To Do")
+
+        with col2:
+            task_status = st.selectbox("Status",["ToDo","Doing","Done"])
+            task_due_date = st.date_input("Due Date")
+
+        if st.button("Add Task"):
+            add_data(task,task_status,task_due_date)
+            st.success("Successully Added Data: {} ".format(task))
+
+    elif choice =="Read":
+        st.subheader("View Items")
+        result = view_all_data()
+        st.write(result)
+        with st.beta_expander("View All Data"):
+            df = pd.DataFrame(result,columns=['Task','Status','Due Date'])
+            st.dataframe(df)
+
+        
+        with st.beta_expander("Task Status"):
+            task_df = df['Status'].value_counts().to_frame()
+            
+            task_df = task_df.reset_index()
+            st.dataframe(task_df)
+
+            pl = px.pie(task_df, names="index", values="Status")
+            st.plotly_chart(pl)
+
+
+
+
+
+
+
+
+    elif choice =="Update":
+        st.subheader("Edit/Ipdate Items")
+        result = view_all_data()
+        df = pd.DataFrame(result,columns=['Task','Status','Due Date'])
+        with st.beta_expander("Current Data"):
+            st.dataframe(df)
+
+        # st.write(view_unique_tasks())
+        list_of_task = [i[0] for i in view_unique_tasks()]
+        # st.write(list_of_task)
+        
+        selected_task = st.selectbox("Task To Edit", list_of_task)
+
+        selected_result = get_task(selected_task)
+        st.write(selected_result)
+        if selected_result:
+            task = selected_result[0][0]
+            task_status = selected_result[0][1]
+            task_due_date = selected_result[0][2]
+
+            # Layout
+            col1, col2 = st.beta_columns(2)
+
+            with col1:
+                new_task = st.text_area("Task To Do", task)
+
+            with col2:
+                new_task_status = st.selectbox(task_status,["ToDo","Doing","Done"])
+                new_task_due_date = st.date_input(task_due_date)
+
+            if st.button("Update Task"):
+                edit_task_data(new_task,new_task_status,new_task_due_date,task,task_status,task_due_date)
+                st.success("Successully Upadated:: {} To ::{}".format(task,new_task))
+
+
+            result2 = view_all_data()
+            df2 = pd.DataFrame(result2,columns=['Task','Status','Due Date'])
+            with st.beta_expander("Updated Data"):
+                st.dataframe(df2)
+
+
+    elif choice =="Delete":
+        st.subheader("Delete Items")
+        result = view_all_data()
+        df = pd.DataFrame(result,columns=['Task','Status','Due Date'])
+        with st.beta_expander("Current Data"):
+            st.dataframe(df)
+
+        list_of_task = [i[0] for i in view_unique_tasks()]    
+        selected_task = st.selectbox("Task To Delete", list_of_task)
+        st.warning("Do You Want to Delete {}?".format(selected_task))
+        if st.button("Delete Task {}".format(selected_task)):
+            delete_data(selected_task)
+            st.success("Task {} has been successfully Delete".format(selected_task))
+
+        new_result = view_all_data()
+        df2 = pd.DataFrame(new_result,columns=['Task','Status','Due Date'])
+        with st.beta_expander("Updated Data"):
+            st.dataframe(df2)
+
+
+    else:
+        st.subheader("About")
+
+
+if __name__=='__main__':
+    main()
